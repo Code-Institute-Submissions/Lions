c27faf0bb6cc84203e419727c0b353b09ac25c7c
FabianMarsh <fabian.marsh00@gmail.com>
2021-02-05 18:29:45 +0000

    set up signup view and basic form

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Subscription
 
 # Create your views here.
@@ -14,3 +15,16 @@ def all_subscriptions(request):
     }
 
     return render(request, "subscriptions/subscriptions.html", context)
+
+
+@login_required
+def subscription_signup(request, sub_id):
+    """ A view to allow users to signup for gym subscriptions """
+
+    sub = get_object_or_404(Subscription, pk=sub_id)
+
+    context = {
+        "subscription": sub
+    }
+
+    return render(request, "subscriptions/subscription_signup.html", context)
