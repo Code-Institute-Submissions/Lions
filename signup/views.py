c2a4496d032d2cf5534266819653d69327d3eede
FabianMarsh <fabian.marsh00@gmail.com>
2021-02-11 18:29:49 +0000

    added stripe elements

--- signup/views.py
+++ signup/views.py
@@ -7,7 +7,8 @@ def signup(request):
     signup_form = SignupForm
     template = "signup/signup.html"
     context = {
-        "signup_form": signup_form
+        "signup_form": signup_form,
+        "stripe_public_key": "pk_test_51IAbyUCU1rp8UcNhz7wyJHGItLEUX2Dlxmo85tGnMF0P2qG7F4Hp4T0gneJNXOPj9Cf3ZcrWH7Vnai8hBvZIITAi00si3GnT38",
     }
 
     return render(request, template, context)
