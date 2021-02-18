0b6c8c4e6bab6ed1f578f334eb80e6a137ff6312
FabianMarsh <fabian.marsh00@gmail.com>
2021-02-18 18:48:16 +0000

    added class methods to webhook handler and webhook view

--- signup/urls.py
+++ signup/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.signup, name="signup"),
     path('<sub_id>/', views.signup, name="signup"),
     path('payment_success/<order_number>', views.payment_success, name="payment_success"),
+    path('wh/', webhook, name="webhook"),
 ]
