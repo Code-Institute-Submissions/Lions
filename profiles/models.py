c6950cd1b55c7dbda2bcfcfcb269d95e15333fcd
FabianMarsh <fabian.marsh00@gmail.com>
2021-02-20 16:58:45 +0000

    updated profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -9,13 +9,24 @@ from django.dispatch import receiver
 class UserProfile(models.Model):
     """
     A user profile model to maintain default
-    billing information and subscriptions history
+    billing information and subscription history
     """
 
+    Choices = [
+        ("male", "Male"),
+        ("female", "Female"),
+        ("other", "Other")
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_email = models.EmailField(max_length=254, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_gender = models.CharField(max_length=10, choices=Choices)
+    default_dob = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
+
 
 # from https://courses.codeinstitute.net/courses/course-v1:CodeInstitute+FSF_102+Q1_2020/courseware/4201818c00aa4ba3a0dae243725f6e32/2c1b98a8efb748009445d5056c97483b/?child=first
 @receiver(post_save, sender=User)
--- signup/models.py
+++ signup/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 
 from subscriptions.models import Subscription
+from profiles.models import UserProfile
 # Create your models here.
 
 
@@ -17,6 +18,7 @@ class SubscriptionDetails(models.Model):
     ]
 
     order_number = models.CharField(max_length=32, null=False, blank=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name="subscriptions")
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
