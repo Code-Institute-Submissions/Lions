587ca62088a4b74e5b6ebc273c4eef8e46685e52
FabianMarsh <fabian.marsh00@gmail.com>
2021-02-21 13:58:03 +0000

    subcription admin - subscription form

---
+++ subscriptions/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Subscription, Category
+
+
+class SubscriptionForm(forms.ModelForm):
+
+    class Meta:
+        model = Subscription
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields["category"].choice = friendly_names
