b3f000177a728331d165f5f56294acb890c3af57
FabianMarsh <fabian.marsh00@gmail.com>
2021-02-27 18:58:10 +0000

    email debugging

--- lions/settings.py
+++ lions/settings.py
@@ -183,7 +183,7 @@ else:
     EMAIL_HOST = "stmp.gmail.com"
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
-    EMAIL_HOST_EMAIL = os.environ.get("EMAIL_HOST_USER")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 if "USE_AWS" in os.environ:
     # Cache Control
