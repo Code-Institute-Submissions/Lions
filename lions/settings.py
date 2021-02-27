dbd32e3d150b4762356ed0eea41637b2203d4b51
FabianMarsh <fabian.marsh00@gmail.com>
2021-02-27 14:37:14 +0000

    sending email functionality

--- lions/settings.py
+++ lions/settings.py
@@ -99,8 +99,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 WSGI_APPLICATION = 'lions.wsgi.application'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
@@ -174,7 +172,18 @@ STRIPE_CURRENCY = "gbp"
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
-DEFAULT_FROM_EMAIL = "lions@example.com"
+
+if "DEVELOPMENT" in os.environ:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "lions@example.com"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_USER_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "stmp.gmail.com"
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    EMAIL_HOST_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 if "USE_AWS" in os.environ:
     # Cache Control
